dba58ae9ef10c7118949ac8be2b38cbb3e40f1b5
Shalmali29 <97212762+Shalmali29@users.noreply.github.com>
2022-01-05 21:39:02 -0800

    Create Grade_claculation.py
    this will calculate and show the grade

---
+++ Grade_claculation.py
@@ -0,0 +1,21 @@
+int p=input("enter the physics marks:")
+int c=input("enter the chemistry marks:")
+int m=input("enter the maths marks:")
+int avg=(p+c+m)/3
+if(avg>=90):
+  print("Distinction")
+else if(avg>=70):
+  print("GOOD")
+else if(avg>=50):
+  print("Average")
+els eif(avg<40):
+  print("FAIL")
+  
+
+
+
+
+
+
+
+<
