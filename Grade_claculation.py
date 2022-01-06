6916b73f47848af721a3e1d95336a033a7c75a1f
Shalmali29 <97212762+Shalmali29@users.noreply.github.com>
2022-01-05 21:53:32 -0800

    Update Grade_claculation.py

--- Grade_claculation.py
+++ Grade_claculation.py
@@ -1,16 +1,17 @@
-int p=input("enter the physics marks:")
-int c=input("enter the chemistry marks:")
-int m=input("enter the maths marks:")
-int avg=(p+c+m)/3
+p =int(input("enter the physics marks:"))
+c =int(input("enter the chemistry marks:"))
+m =int(input("enter the maths marks:"))
+avg=(p+c+m)/3
 if(avg>=90):
   print("Distinction")
-else if(avg>=70):
+elif(avg>=70):
   print("GOOD")
-else if(avg>=50):
+elif(avg>=50):
   print("Average")
-els eif(avg<40):
+elif(avg>=40):
+    print("pass")
+elif(avg<40):
   print("FAIL")
-  
 
 
 
